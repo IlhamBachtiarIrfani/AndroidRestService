f5f9e9735329d900eb507829a0e1db51f99a5d87
Ilham Irfan <42286970+IlhamBachtiarIrfani@users.noreply.github.com>
2022-12-02 22:58:58 +0700

    rest service

---
+++ app/src/main/java/com/ilhamirfan/networking/model/AddMahasiswaResponse.java
@@ -0,0 +1,19 @@
+package com.ilhamirfan.networking.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AddMahasiswaResponse {
+    @SerializedName("message")
+    private String message;
+
+    @SerializedName("status")
+    private boolean status;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+}
